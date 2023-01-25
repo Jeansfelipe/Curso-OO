b91f249ee36f5c3a611f6a4e2174904a9e34a25d
Jean Felipe <jeanfelipe214@hotmail.com>
2023-01-25 20:30:51 -0300

    Exercicio de implementaçao do intervalo e exercicio da soma

---
+++ AloMundo/src/ExercicioImprimeIntervalo.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class ExercicioImprimeIntervalo {
+
+	public static void main(String[] args) {
+		int valor,contador;
+		contador = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o valor de inicio: "));
+		valor =Integer.parseInt(JOptionPane.showInputDialog(null, "Digite o valor final: "));
+		
+		int contNum = 0;
+		while(contador != (valor+1))
+		{
+			JOptionPane.showMessageDialog(null, "O "+ (contNum+1) + " º numero eh: "+ contador);
+			contador++;
+			contNum++;
+		}
+	}
+
+}
---
+++ AloMundo/src/ExercicioSoma.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class ExercicioSoma {
+
+	public static void main(String[] args) {
+		int soma = 0, quantNum=0;
+		int valor = 0, media = 0;
+		
+		while(valor != -1)
+		{
+			valor =Integer.parseInt(JOptionPane.showInputDialog(null, "Digite um valor: "));
+			if(valor != -1)
+			{
+				soma = soma + valor;
+				quantNum++;
+			}
+		}
+		
+		media = soma/quantNum;
+		JOptionPane.showMessageDialog(null, "a media eh: "+ media);
+
+	}
+
+}
